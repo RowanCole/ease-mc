512eb1b48898998deec2f0971c312718bcbf16a0
Rowan <qc20702@hotmail.com>
2026-08-06 15:04:48 +0800

    reduce bundle size

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -8,8 +8,6 @@ mod jre;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             game::launch_game,
             game::close_game,
